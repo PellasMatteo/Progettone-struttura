1b9fd9623c9f4590c4b23654f226d47d9fdc14bb
Matteo Pellas <matteopellas36@gmail.com>
2024-04-03 14:41:44 +0000

    inzio

---
+++ progettone-struttura-main/client/src/app/animal/animal.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input } from '@angular/core';
+import { Animal, AnimalsService } from '../animals.service';
+import { FoodData } from 'src/models/foodData.model';
+
+@Component({
+  selector: 'app-animal',
+  templateUrl: './animal.component.html',
+  styleUrls: ['./animal.component.css']
+})
+
+
+export class AnimalComponent {
+
+  @Input() animal : Animal = undefined!
+  foodData : FoodData = undefined!
+  
+  constructor(private animalService : AnimalsService) { }
+
+  nutri()
+  {
+    this.animalService.nutri(this.animal).subscribe(
+      data=> this.foodData = data
+    )
+  }
+}
